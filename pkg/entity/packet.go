fb2f849b04704e5f2b83b2c29d0a8881fa196a2b
julic98 <julioc.santos98@gmail.com>
2018-12-15 21:09:21 -0200

    add filter fields

--- pkg/entity/packet.go
+++ pkg/entity/packet.go
@@ -4,11 +4,14 @@ import "time"
 
 // Packet ...
 type Packet struct {
-	ID              ID        `json:"id,omitempty"`
-	Weight          float64   `json:"weight,omitempty"`
-	Deadline        time.Time `json:"deadline,omitempty"`
-	DeliveryAddress string    `json:"delivery_address,omitempty"`
-	IDUser          ID        `json:"id_user,omitempty"`
-	RouteStart      string    `json:"route_start,omitempty"`
-	RouteEnd        string    `json:"route_end,omitempty"`
+	ID                 ID        `json:"id,omitempty"`
+	Weight             float64   `json:"weight,omitempty"`
+	Deadline           time.Time `json:"deadline,omitempty"`
+	DeliveryAddress    string    `json:"delivery_address,omitempty"`
+	IDUser             ID        `json:"id_user,omitempty"`
+	RouteStart         string    `json:"route_start,omitempty"`
+	RouteEnd           string    `json:"route_end,omitempty"`
+	Delivered          bool      `json:"delivered,omitempty"`
+	Delivering         bool      `json:"delivering,omitempty"`
+	DistributionCenter bool      `json:"distribution_center,omitempty"`
 }
